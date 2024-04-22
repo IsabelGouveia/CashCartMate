39581f81656ba999c5ff203e3e02268464ec8d4c
Isabel Gouveia <isabelgouveia@hotmail.com>
2024-04-22 19:58:00 +0100

    User model created

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_04_19_163726) do
+ActiveRecord::Schema[7.1].define(version: 2024_04_22_185640) do
   create_table "cart_items", force: :cascade do |t|
     t.integer "cart_id", null: false
     t.integer "product_id", null: false
@@ -32,6 +32,11 @@ ActiveRecord::Schema[7.1].define(version: 2024_04_19_163726) do
     t.string "code", null: false
     t.string "name", null: false
     t.decimal "price", precision: 8, scale: 2, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
